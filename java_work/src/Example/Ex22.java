008cb2312bed6f2ee14e5c77d74db0356e5117ca
dudgns1170@naver.com <dudgns9466!>
2021-05-12 17:11:09 +0900

    5/12일 자바 실습

---
+++ java_work/src/Example/Ex22.java
@@ -0,0 +1,75 @@
+package Example;
+
+import java.util.Scanner;
+
+public class Ex22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//5/12일 문제 풀이
+		//1번 입력한 개수 만큼 "*"표시
+		Scanner sc = new Scanner(System.in);
+		System.out.println("입력");
+//		int input = sc.nextInt();
+		
+		/*if(input > 0) {
+		for(int i =0; i< input; i++)
+		{
+			if(i % 4 == 0)
+				System.out.print("\n*");
+//			else if (i % 5 == 10)
+//				System.out.print("*");
+//			else
+//				System.out.print("*");
+		
+		}
+		if(input % 5 != 0)
+			System.out.println();
+		}
+		sc.close();
+*/
+		
+		//2번 입력한 정수 값의 모든 약수 와 그 개수를 표시
+		//약수 구하는 공식은 입력값 / 1 ==0 으로 생각하면 빠를거 같습니다.
+//		int sum= 0;
+//		for(int a=1; a <= input; a++)
+//		{	if(input%a==0)
+//				{System.out.println(a);
+//				sum++;
+//				}
+//	
+//	}System.out.println("총 개수는:"+sum);
+		//3번 입력값을 1부터n까지의 정수값에 각 값을 제곱해서 표시하는 프로그램
+//		for(int i =1; i<= input; i++)
+//			System.out.println(i+"의 제곱은"+i*i);
+		
+		//4번 월 을 정수 값으로 입력반은 후 해당 달 의 계절을 표시
+		
+//		int i =0;
+//		do {
+//			int input = sc.nextInt();
+//			if(input <1 || input >12)
+//				System.out.println("다시입력");
+//			else if (input >=3 && input <=5)
+//				System.out.println("봄");
+//			else if (input >=6 && input <=8)
+//				System.out.println("여름");
+//			else if (input >=9 && input <=11)
+//				System.out.println("가을");
+//			else
+//				System.out.println("겨울");
+//			
+//			System.out.println("재시작 1 no 0");
+//			i = sc.nextInt();
+//			System.out.println("d월");
+//		}while(i == 1 );
+		
+		//5번 
+		
+		
+			
+		
+		
+
+}
+}
