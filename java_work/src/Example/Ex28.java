4d94a6f32d9490a1509c2dda6eea6e576575824b
dudgns1170 <dudgns1170@naver.com>
2021-05-24 15:17:46 +0900

    5/24일 java 문제풀이

---
+++ java_work/src/Example/Ex28.java
@@ -0,0 +1,43 @@
+package Example;
+
+import java.util.Scanner;
+
+public class Ex28 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		5/24일 문제 풀이
+//		3개의 정수 값을 읽어서 합계와 평균을 구하는 프로그램을 작성.평균을 실수로 구하세요
+		/*Scanner sc =new Scanner(System.in);
+		System.out.println("몇개의 숫자를 더할까여?");
+		int input = sc.nextInt();
+		int sum = 0;
+		double i =0;
+		for(i =0; i<input; i++)
+		{
+			System.out.println("숫자입력");
+			double v =sc.nextDouble();
+			sum += v;
+		}
+		System.out.println("합:"+sum);
+		System.out.println("평균"+(sum/i));
+	}*/
+//		int형 변수에 실수값을 대입하여 표시.(강제 형변환)
+//	Scanner sc = new Scanner(System.in);
+//	System.out.println("숫자");
+//	int a= (int) sc.nextDouble();
+
+		
+//		3번 float형 변수를 0.0부터 1.0까지 0.001씩 증가표시
+//		그옆으로 int 형 변수 0부터 1000까지 증가시키며 1000으로 나눈값 을 표시
+		
+//		float x= 0.0F;
+//		for(int i =0; i <= 1000; i++)
+//		{
+//			System.out.printf("%9.7f       %9.7f\n",x,(float)i/1000);
+//			x+= 0.001F;
+//		}
+	}
+
+}
