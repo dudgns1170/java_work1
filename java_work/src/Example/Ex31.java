ede6ff5204d19889651f7bc7945c86f716a45c11
dudgns1170 <dudgns1170@naver.com>
2021-06-07 18:56:38 +0900

    6/7

---
+++ java_work/src/Example/Ex31.java
@@ -0,0 +1,37 @@
+package Example;
+
+import java.util.Scanner;
+
+public class Ex31 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		6/7일 문제풀이
+//		배열 요소를입력바다 특정값을 가지는 요소를 찾으시오
+		Scanner sc = new Scanner(System.in);
+		
+		/*System.out.println("요소수 입력:");
+		int p= sc.nextInt();
+		int [] a = new int [p];
+		int i ;
+		for(i =0; i<a.length; i++)
+			{System.out.println("a["+i+"]=");
+			a[i]= sc.nextInt();
+			}
+		System.out.println("찾을 숫자");
+		int v =sc.nextInt();
+		for(i =0; i<a.length; i++)
+//		for(i =p-1; i>= 0 ; i--)  뒤 에있는 값부터 찾을경우 
+		{
+			if(v == a[i])
+				{System.out.println("찾는 숫자는 :"+ "a["+ i +"]에 있습니다");
+				break;
+				}
+			
+		}*/
+		
+		
+
+	}
+
+}
